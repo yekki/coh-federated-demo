e551c2732a0bbf16bdaec6083887709f4acb5620
Gary Niu <gary.niu@gmail.com>
2016-07-28 21:31:03 +0800

    update env vars

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export JAVA_HOME=/Users/gniu/.jenv/versions/1.8
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_92.jdk/Contents/Home
 export COHERENCE_HOME=/Users/gniu/Oracle/mw12c/coherence
-export DEMO_HOME=/Users/gniu/Workspaces/ProjectStuff/FederatedCachingDemo
+export DEMO_HOME=/Users/gniu/Workspaces/coh-federated-demo
 export COH_OPTS="-Djava.net.preferIPv4Stack=true"
 export CLASSPATH=$DEMO_HOME/classes:$DEMO_HOME/config:$COHERENCE_HOME/lib/coherence.jar:$COHERENCE_HOME/lib/jline.jar
